c4236de623ec1010d039f08a492a68b7190dc705
iSTORMERi <294786132+iSTORMERi@users.noreply.github.com>
2026-07-20 20:41:30 +0300

    fix: update script src path and vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // Обязательно укажи название твоего репозитория со слэшами по бокам!
   base: '/evolution-simulator/',
-  build: {
-    outDir: 'dist',
-  },
 });
