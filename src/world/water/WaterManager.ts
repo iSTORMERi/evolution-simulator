0e045ce951f716d6518a5cffd6d541637d7f3f44
iSTORMERi <294786132+iSTORMERi@users.noreply.github.com>
2026-07-21 01:46:12 +0300

    caustic

--- src/world/water/WaterManager.ts
+++ src/world/water/WaterManager.ts
@@ -1,26 +1,33 @@
 import * as PIXI from 'pixi.js';
 import { CoastalWaterController } from './ocean/CoastalWaterController';
+import { OceanGlintsController } from './ocean/OceanGlintsController';
 
 export class WaterManager {
   public container: PIXI.Container;
 
   private coastalController: CoastalWaterController;
+  private glintsController: OceanGlintsController;
 
   constructor(mapWidth: number, mapHeight: number, coastalRatio: number) {
     this.container = new PIXI.Container();
 
-    // Инициализируем контроллер прибрежной воды
+    // Инициализируем контроллеры
     this.coastalController = new CoastalWaterController(mapWidth, mapHeight, coastalRatio);
+    // 60 точек дадут красивое мягкое мерцание и 0 нагрузки на CPU/GPU
+    this.glintsController = new OceanGlintsController(mapWidth, mapHeight, coastalRatio, 60);
 
-    // Добавляем его контейнер в менеджер
+    // Добавляем берег, а затем поверх воды -- солнечные блики
     this.container.addChild(this.coastalController.container);
+    this.container.addChild(this.glintsController.container);
   }
 
   public updateTimeState(hours: number): void {
     this.coastalController.updateTimeState(hours);
+    this.glintsController.updateTimeState(hours);
   }
 
   public update(deltaSeconds: number): void {
     this.coastalController.update(deltaSeconds);
+    this.glintsController.update(deltaSeconds);
   }
 }
